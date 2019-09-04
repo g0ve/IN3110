dc2700c74d10f8d15e98f067b5398607a993d8cb
Patrick Kvistbråten Gove <patrikg@student.matnat.uio.no>
2019-09-04 13:00:03 +0200

    Added comments

--- assignment2/climb.sh
+++ assignment2/climb.sh
@@ -4,14 +4,14 @@ function climb (){
   declare -i n
   declare -i i
 
-if [[ $# -gt 0 ]]; then
-  n=$1
-  i=0
-  while [[ "$i" != "$n" ]]; do
+if [[ $# -gt 0 ]]; then #If number of arguments are greater then 0...run
+  n=$1 #Number of climbs
+  i=0 #Counter
+  while [[ "$i" != "$n" ]]; do #While counter is not equal to number of climbs. Then cd ../
     cd ../
     i=$i+1
   done
-else
+else #If there is no arguments climb just 1 time
   cd ../
 fi
 }
--- assignment2/track.sh
+++ assignment2/track.sh
@@ -1,6 +1,7 @@
 #!/bin/bash -x
 
 function track() {
+  #All varibels used declared
   logfile="LOGFILE.txt"
   command=$1
   label=$2
@@ -11,7 +12,7 @@ function track() {
   declare -i diffrence
   taskCounter=1
 
-  #Get file, and check if it exists. If not end program.
+  #Get file, and check if it exists. If not creates logfil for you
   if [[ -f $logfile ]]; then
     echo "File exists and is a regular file."
   else
@@ -19,13 +20,14 @@ function track() {
     touch $logfile
   fi
 
-  #Runs if there is more then 0 arguments
-
+  #I wanted to try and use case instead of if-sentences
+  #If command(first argument) is "start" then do a task, same goes with stop, status and log
   case "$command" in
-    "start")
-    if [[ $label == "" ]]; then
+
+    "start") #track start [label]
+    if [[ $label == "" ]]; then #If there is no label, ask user to add a second parameter
       echo "Function track start needs 2 parameters: -track start [label]"
-    elif [[ $lastLineInLogfile == "" || $lastLineInLogfile == END* ]]; then
+    elif [[ $lastLineInLogfile == "" || $lastLineInLogfile == END* ]]; then #elif last line in LOGFILE is an empty line or it starts with END, you can run a task
       echo "START $(date)" | tee -a $logfile
       echo "LABEL This is: ${label}" | tee -a $logfile
     else
@@ -33,8 +35,8 @@ function track() {
     fi
     ;;
 
-    "stop")
-    if [[ $lastLineInLogfile == LABEL* ]]; then
+    "stop") #track stop
+    if [[ $lastLineInLogfile == LABEL* ]]; then #if last line in LOGFILE starts with label means a task is running and you can stop it
       echo "END $(date)" | tee -a $logfile
       echo "" | tee -a $logfile
     else
@@ -43,7 +45,7 @@ function track() {
     ;;
 
     "status")
-    if [[ $lastLineInLogfile == LABEL* ]]; then
+    if [[ $lastLineInLogfile == LABEL* ]]; then #same has "stop", but here it removes the 6 first characters in the last line
       echo "STATUS ${lastLineInLogfile:6}"  #| tee -a $logfile
     else
       echo "No active task"
@@ -51,32 +53,31 @@ function track() {
     ;;
 
     "log")
+    while read lines; do #Reads the LOGFILE
 
-    while read lines; do
-      if [[ $lines == START* ]]; then
+      if [[ $lines == START* ]]; then #If a line starts with START, remove 6 first characters and declare rest of line to a varibel
         start="$(echo $lines | grep "START")"
         startDate=${start:6}
-        startSec=$(date -d "$startDate" +%s)
+        startSec=$(date -d "$startDate" +%s) #With %s we can convert a "date" type to seconds.
 
       elif [[ $lines == END* ]]; then
-        end="$(echo $lines | grep "END")"
+        end="$(echo $lines | grep "END")" #Does the same has START first, but it gets the END date instead
         endDate=${end:4}
         endSec=$(date -d "$endDate" +%s)
 
+        #Calculate the diffrence in seconds between end and start
         diffrence=endSec-startSec
-        convertedDiff=$(date -u -d @${diffrence} +%T)
+        convertedDiff=$(date -u -d @${diffrence} +%T) #With %T we can convert a "date" type to this format: HH:MM:SS
         echo "Task ${taskCounter}: ${convertedDiff}"
         ((taskCounter++))
       fi
 
-
     done < $logfile
     ;;
 
-    *)
-
-    echo "$(<README.md)"
-
+    *) #If none of the cases work this runs.
+    echo "$(<README.md)" #Prints out how to use the program to user from README file
+    ;;
+    
   esac
-
 }
