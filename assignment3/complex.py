1248090e4b6e9841639c613e351b6d46d91a73d1
Patrick Kvistbråten Gove <patrikg@student.matnat.uio.no>
2019-09-10 13:07:08 +0200

    3.2 and 3.3 is done
    Jeg er kokken din ;)
    
    started on 3.4

--- assignment3/complex.py
+++ assignment3/complex.py
@@ -2,17 +2,68 @@
 
 import math
 
-class Complex():
+class Complex:
+
+
     """Complex works almost same has the complex class buildt in python"""
 
     def __init__(self, real, imaginary):
         self.real = real
         self.imaginary = imaginary
 
+    # Assignment 3.3
+
+    def conjugate(self):
+        x = self.real
+        y = self.imaginary * -1
+        return Complex(x, y)
+
+    def modulus(self):
+        x = self.real**2
+        y = self.imaginary**2
+        return math.sqrt(x + y)
+
     def __add__(self, other):
-        return Complex(self.real + other.real, self.imag + other.imag)
+        x = self.real + other.real
+        y = self.imaginary + other.imaginary
+        return Complex(x, y)
 
     def __sub__(self, other):
-        return Complex(self.real - other.real, self.imag - other.imag)
+        x=self.real - other.real
+        y=self.imaginary - other.imaginary
+        return Complex(x, y)
 
-            
+    def __mul__(self, other):
+        x = self.real * other.real - self.imaginary * other.imaginary
+        y = self.real * other.imaginary + self.imaginary * other.real
+        return Complex(x, y)
+
+    def __eq__(self, other):
+        realBoolean = self.real == other.real
+        imaginaryBoolean = self.imaginary == other.imaginary
+
+        return realBoolean and imaginaryBoolean
+
+    # Assignment 3.4
+    def __radd__(self, other):
+        return __add__(self, other)
+
+    def __rsub__(self, other):
+        return __sub__(self, other)
+
+    def __rmul__(self, other):
+        return __mul__(self, other)
+
+
+    # Optional, possibly useful methods
+
+    # Allows you to write `-a`
+    def __neg__(self):
+        x = self.real * -1
+        y = self.imaginary * -1
+        return Complex(x, y)
+
+    # Make the `complex` function turn this into Python's version of a complex number
+    def __complex__(self):
+        x = self.real
+        y = self.imaginary * 1j
--- assignment3/test_complex.py
+++ assignment3/test_complex.py
@@ -1,9 +1,57 @@
 #!/usr/bin/env python
 
+from complex import Complex
+import math
+
+"""
+Tests if __con__ works has it should
+"""
+def test_con():
+
+    x = Complex(1, 2)
+    #1 - 2i
+    assert x.conjugate() == Complex(1, -2)
+
+    x = Complex(5, -6)
+    #5 + 6i
+    assert x.conjugate() == Complex(5, 6)
+
+    x = Complex(0, -9)
+    #9i
+    assert x.conjugate() == Complex(0, 9)
+
+    x = Complex(0, 4)
+    #-4i
+    assert x.conjugate() == Complex(0, -4)
+
+    x = Complex(10, 0)
+    #10
+    assert x.conjugate() == Complex(10, 0)
+
+"""
+Tests if __mod__ works has it should
+"""
+def test_mod():
+    x = Complex(1, 7)
+    #7.0710
+    assert x.modulus() == math.sqrt(50)
+
+    x = Complex(-3, 4)
+    #5
+    assert x.modulus() == math.sqrt(25)
+
+    x = Complex(3, -5)
+    #5.8309
+    assert x.modulus() == math.sqrt(34)
+
+    x = Complex(-6, -4)
+    #7.2111
+    assert x.modulus() == math.sqrt(52)
+
 """
 Tests if __add__ works has it should
 """
-def add_test():
+def test_add():
     x = Complex(1, 9)
     y = Complex(7, 5)
     assert (x + y) == Complex(8, 14) # 8+14i
@@ -35,17 +83,17 @@ def add_test():
 """
 Tests if __sub__ works has it should
 """
-def sub_test():
+def test_sub():
     x = Complex(1, 9)
     y = Complex(7, 5)
-    assert (x - y) == Complex(8, 14) # 8-14i
+    assert (x - y) == Complex(-6, 4) # 8-14i
 
     x = Complex(0, 0)
     y = Complex(0, 0)
     assert (x - y) == Complex(0, 0) # 0
 
-    x = Complex(9, 0)
-    y = Complex(20, 0)
+    x = Complex(20, 0)
+    y = Complex(9, 0)
     assert (x - y) == Complex(11, 0) # 11
 
     x = Complex(0, 11)
@@ -65,45 +113,36 @@ def sub_test():
     assert (x - y) == Complex(0, -13) # -13i
 
 """
-Tests if __con__ works has it should
+Tests if __mul__ works has it should
 """
-def con_test():
+def test_mul():
+    #x = self.real * other.real - self.imaginary * other.imaginary
+    #y = self.real * other.imaginary + self.imaginary * other.real
 
-    x = Complex(1, 2)
-    assert x.__con__() == Complex(1, 2) # 1 - 2i
+    x = Complex(4, 6)
+    y = Complex(2, 1)
+    #32i
+    assert (x * y) == Complex(2, 16)
 
-    x = Complex(-3, 4)
-    assert x.__con__() == Complex(-3, 4) # -3 + 4i
+    x = Complex(-4, -6)
+    y = Complex(2, 1)
+    #-2-16i
+    assert (x * y) == Complex(-2, -16)
 
-    x = Complex(5, -6)
-    assert x.__con__() == Complex(5, -6) # 5 + 6i
-
-    x = Complex(-7, -8)
-    assert x.__con__() == Complex(-7, -8) # -7 - 8i
-
-    x = Complex(0, 9)
-    assert x.__con__() == Complex(0, 9) # 9i
-
-    x = Complex(10, 0)
-    assert x.__con__() == Complex(10, 0) # 10
+    x = Complex(2, 1)
+    y = Complex(4, -3)
+    #12-2i
+    assert (x * y) == Complex(11, -2)
 
     x = Complex(0, 0)
-    assert x.__con__() == Complex(0, 0) # 0
-
-"""
-Tests if __mod__ works has it should
-"""
-def mod_test():
-    x = Complex(-3, 4)
-    assert x.__mod__() == math.sqrt(25)
-
-    x = Complex(0, 0)
-    assert x.__mod__() == 0
+    y = Complex(0, 0)
+    #0
+    assert (x * y) == Complex(0, 0)
 
 """
 Tests if __eq__ works has it should
 """
-def eq_test():
+def test_eq():
     x = Complex(1, 2)
     assert x == Complex(1, 2)
 
