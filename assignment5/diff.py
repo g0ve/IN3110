8ba6d99d1a0783bb7ddd7d52af315af3e094d5ad
Patrick Kvistbråten Gove <patrikg@student.matnat.uio.no>
2019-10-24 17:14:14 +0200

    diff done maybe?

--- assignment5/diff.py
+++ assignment5/diff.py
@@ -1,14 +1,48 @@
 import sys
 import glob
+import itertools
+
 
 def main(input_file, output_file):
 
     with open(input_file, 'r') as iFile:
-        ogVersion = iFile.read()
+        ogVersion = iFile.read().split('\n')
     with open(output_file, 'r') as oFile:
-        modVersion = oFile.read()
+        modVersion = oFile.read().split('\n')
 
+    ogLineSet = set(ogVersion)
+    modLineSet = set(modVersion)
 
+    ogAdded = ogLineSet - modLineSet
+    ogRemoved = modLineSet - ogLineSet
+    similarities = modLineSet & ogLineSet
+
+    ogLenght = len(ogVersion)
+    modLenght = len(modVersion)
+    lenght = 0
+    i = 0
+
+    if ogLenght > modLenght:
+        lenght = ogLenght
+    else:
+        lenght = modLenght
+
+    while i < lenght:
+
+        if i < ogLenght:
+            if ogVersion[i] in similarities:
+                print('0 ' + ogVersion[i])
+            if ogVersion[i] in ogAdded:
+                print('- ' + ogVersion[i].strip())
+            elif ogVersion[i] in ogRemoved:
+                print('+ ' + ogVersion[i].strip())
+        if i < modLenght:
+            if modVersion[i] in ogAdded:
+                print('- ' + modVersion[i].strip())
+            elif modVersion[i] in ogRemoved:
+                print('+ ' + modVersion[i].strip())
+
+        i += 1
 
 if __name__ == '__main__':
     if len(sys.argv) == 3:
