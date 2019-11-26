eef9978d268da1fbaee4707f962cbeb0c53c2702
Patrick Kvistbråten Gove <patrikg@student.matnat.uio.no>
2019-11-26 14:49:29 +0100

    Update fitting.py
    Just doing some random test

--- assignment6/fitting.py
+++ assignment6/fitting.py
@@ -12,11 +12,13 @@ from sklearn.neighbors import KNeighborsClassifier
 from sklearn.linear_model import LogisticRegression
 
 
-def fit(filename, feature):
+def fitKNN(filename, feature1, feature2):
     # df = data.read_csv(filename)
     # df = df.drop(columns=["diabetes"])
 
+
     sets = data.make_sets(filename)
+
 
     training_set = sets[0]
     validation_set = sets[1]
@@ -24,26 +26,24 @@ def fit(filename, feature):
     training_set = training_set.drop(columns=["diabetes"])
     validation_set = validation_set.drop(columns=["diabetes"])
 
-    Y_train = training_set[feature].values #Bare feature
-    X_train = training_set.drop(columns=[feature]) #Hele
+    Y_train = training_set[feature2].values #Bare feature
+    X_train = training_set.drop(columns=[feature1]) #Hele
 
-    Y_valid = validation_set[feature].values #Feature
-    X_valid = validation_set.drop(columns=[feature]) #Hele
-
-
+    Y_valid = validation_set[feature2].values #Feature
+    X_valid = validation_set.drop(columns=[feature1]) #Hele
 
     # print(df)
-    # x1 = df.drop(columns=[feature])
-    # y1 = df[feature].values
-    #
-    #
+    # x1 = training_set.drop(columns=[feature])
+    # y1 = validation_set[feature].values
+
+
     # X_train, X_valid, Y_train, Y_valid = train_test_split(x1, y1, test_size=0.30, random_state=1)
 
 
     # print(X_train)
     # print(Y_train)
 
-    knn = KNeighborsClassifier(n_neighbors = 4)
+    knn = KNeighborsClassifier(n_neighbors = 1)
     knn.fit(X_train, Y_train)
     pred_target = knn.predict(X_valid)
     # score = metrics.accuracy_score(Y_valid, pred_target)
@@ -56,6 +56,6 @@ if __name__ == '__main__':
         lstFiles = glob.glob(sys.argv[1])
     if all(lstFiles):
         print("Found all files :)\n")
-        fit(sys.argv[1], 'pressure')
+        fitKNN(sys.argv[1], 'mass', 'glucose')
     else:
         print("Cant find file/image. Make sure file/image is in your directory")
